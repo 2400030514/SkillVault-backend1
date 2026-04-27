479eb6a6664444415051f97a52fbc725118d9345
NUNNA HASINI <nunnahasini7@gmail.com>
2026-04-27 22:33:29 +0530

    Update AuthController.java

--- src/main/java/com/skillvault/controller/AuthController.java
+++ src/main/java/com/skillvault/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "https://skillvault-frontend1-production.up.railway.app/", maxAge = 3600)
 
 @RestController
 @RequestMapping("/api/auth")
